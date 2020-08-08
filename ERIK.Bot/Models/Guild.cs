efe4b35035b0f6c1ddb2bdc2f5e39430dfb3dd2d
Enes <mail.enes.nu@gmail.com>
2020-08-08 13:08:46 +0200

    Update database, use longs instead of ulongs for db

--- ERIK.Bot/Models/Guild.cs
+++ ERIK.Bot/Models/Guild.cs
@@ -1,11 +1,36 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ERIK.Bot.Models
 {
     public class Guild
     {
         [Key]
-        public ulong Id { get; set; }
+        public long __Id { get; set; }
+
+
+        // Access/modify this variable instead.
+        // Tell EF not to map this field to a Db table
+        [NotMapped]
+        public ulong Id
+        {
+            get
+            {
+                unchecked
+                {
+                    return (ulong)__Id;
+                }
+            }
+
+            set
+            {
+                unchecked
+                {
+                    __Id = (long)value;
+                }
+            }
+        }
+
         public string Prefix { get; set; }
         public ulong LfgPrepublishChannelId { get; set; }
         public ulong LfgPublishChannelId { get; set; }
