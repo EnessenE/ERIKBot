5e3f0bc291f98cddc85b6d43accdb8386a3cc6f1
Enes <mail.enes.nu@gmail.com>
2020-08-09 12:22:45 +0200

    Update EntityContext.cs

--- ERIK.Bot/Context/EntityContext.cs
+++ ERIK.Bot/Context/EntityContext.cs
@@ -66,13 +66,15 @@ namespace ERIK.Bot.Context
 
         public async Task<List<SavedMessage>> GetAllNonPublished(ulong guildId)
         {
-            var result = SavedMessages.Where(a => a.Published == false && a.GuildId == guildId).Include(a => a.Reactions).Include(a => a.TrackedIds).ToList();
+            var result = SavedMessages.Where(a => a.Published == false && a.GuildId == guildId).Include(a => a.TrackedIds).Include(x => x.Reactions)
+                .ThenInclude(x => x.User).Include(a => a.TrackedIds).ToList();
             return result;
         }
 
         public async Task<List<SavedMessage>> GetAllNonPublished()
         {
-            var result = SavedMessages.Where(a => a.Published == false).Include(a => a.Reactions).Include(a => a.TrackedIds).ToList();
+            var result = SavedMessages.Where(a => a.Published == false).Include(a => a.TrackedIds).Include(x => x.Reactions)
+                .ThenInclude(x => x.User).Include(a => a.TrackedIds).ToList();
             return result;
         }
 
