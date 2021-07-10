32bb3f874d277aed035b6b3309c2bf9df52b82f0
Enes <9263360+EnessenE@users.noreply.github.com>
2021-07-10 22:36:04 +0200

    Update commands.py

--- ERIK.Bot.Python/commands.py
+++ ERIK.Bot.Python/commands.py
@@ -1,9 +1,9 @@
 from discord_slash.context import ComponentContext
 from loader import *
-from discord_slash.utils.manage_commands import create_option
+from discord_slash.utils.manage_commands import create_option, create_permission
 import numpy as np
 from discord_slash.utils.manage_components import create_button, create_actionrow
-from discord_slash.model import ButtonStyle
+from discord_slash.model import ButtonStyle, SlashCommandPermissionType
 from discord_slash.utils.manage_components import wait_for_component, spread_to_rows
 
 ApexCharacters = [
@@ -28,6 +28,7 @@ ApexCharacters = [
 ApexSentence = ['Have you tried', 'How about', 'Try', 'Maybe', 'Ever tried', '', 'Maybe try', 'Just play',
                 'Have you tried pressing Q quicker with', 'Slide more as', 'Abuse the charge tower with', 'Lose your rp with']
 
+rolebuttons = []
 
 class Commands():
     def __init__(self):
@@ -50,32 +51,53 @@ class Commands():
     async def ping(ctx):
         await ctx.send(f'Pong! ({client.latency*1000}ms)')
 
-    @slash.slash(name='role', description="Add or remove yourself from a gamerole", guild_ids=guild_ids)
+    @slash.slash(name='role', description="Add or remove yourself from a gamerole",
+        #     permissions={
+        #       250621419325489153: [
+        #         create_permission(287639043351773184, SlashCommandPermissionType.ROLE, True),
+        #       ]
+        #    }
+           )
     async def rolesetter(ctx):
         roles = ctx.guild.roles
-        rolebuttons = []
         rows = []
+        tempButtons = []
 
+        rolecount = 0
         for role in roles:
-            button = create_button(style = ButtonStyle.green, label = role.name)
-            rolebuttons.append(button)
+            rolecount+=1
+            if rolecount < 25 and "?!" in role.name:
+                button = create_button(style = ButtonStyle.green, label = role.name)
+                rolebuttons.append(button)
+                tempButtons.append(button)
 
         # round(rolebuttons.count / 5)
-        rows = spread_to_rows(*rolebuttons)
+        rows = spread_to_rows(*tempButtons)
 
         await ctx.send("Choose the role you want to change", components=rows)
         
-        button_ctx: ComponentContext = await wait_for_component(client, components=rows)
-        await button_ctx.origin_message.add_reaction("🤔")
+    async def HandleInteraction(self, button_ctx: ComponentContext):
+        # button_ctx: ComponentContext = await wait_for_component(client, components=rows)
+        await button_ctx.defer()
+
+        # await button_ctx.origin_message.add_reaction("🤔")
+
+        roles = button_ctx.guild.roles
 
         chosenButton = next(rolebutton for rolebutton in rolebuttons if rolebutton["custom_id"] == button_ctx.custom_id )
         chosenRole = next(role for role in roles if role.name == chosenButton["label"])
         interacter = button_ctx.author
+        reasoning = "Requested through the slash command /role"
 
         print(f"{interacter.display_name} wants to change role {chosenRole.name}")
-        await interacter.add_roles(chosenRole, reason= "Requested through the slash command /role")
-        await button_ctx.origin_message.add_reaction("✅")
-        
+        text = "??"
+        if (chosenRole in interacter.roles):
+            await interacter.remove_roles(chosenRole, reason = reasoning)
+            text = f"{interacter.display_name} - {chosenRole.name} ✅"
+        else:
+            await interacter.add_roles(chosenRole, reason= reasoning)
+            text = f"{interacter.display_name} + {chosenRole.name} ✅"
+        await button_ctx.send(text, hidden = True)
 
     @slash.slash(name='apex', description="Returns a random apex character for you to play.")
     async def ping(ctx):
