b08ea59c9d6ebba12bf83d0f8207459b0919c36f
Enes <9263360+EnessenE@users.noreply.github.com>
2021-07-13 13:33:24 +0200

    Add json re-reading

--- ERIK.Bot.Python/cloud.py
+++ ERIK.Bot.Python/cloud.py
@@ -1,4 +1,5 @@
 import collections
+import os
 import sys
 from typing import Dict, List
 import json
@@ -43,11 +44,12 @@ async def server_messages(server: discord.Guild, to_edit: discord.Message) -> li
 		server.text_channels
 	))
 	for i, channel in tqdm(list(enumerate(channels)), desc=f"Channels {server.name}", file=sys.stdout):
-		await to_edit.edit(content=f"> Reading {channel.mention} ({i+1}/{len(channels)}) (total messages: {len(messages)})")
+		print(channel.name)
+		print(len(messages))
+		await to_edit.edit(content=f"> Reading {channel.mention} ({i+1}/{len(channels)}) (total up til now: {len(messages)} messages)")
 		# for every message in the channel up to a limit
 		async for message in tqdm_asyncio(
-				channel.history(after=date_after), desc=channel.name,
-				file=sys.stdout, leave=False
+				channel.history(limit=100000, after=date_after), desc=channel.name, file=sys.stdout, leave=False
 		):
 			if not message.author.bot:
 				messages.append((message.author.id, message.content))
@@ -72,67 +74,91 @@ async def add_server(server: discord.Guild, messages):
 
 
 async def load(ctx, days=None):
-	finalmessages = list()
-	await ctx.channel.send(f"Loading messages since the server creation")
-	to_edit = await ctx.channel.send(".")
+	finalmessages = []
+	await ctx.channel.send(f"Loading messages since the server creation", delete_after=20 * 60)
+	to_edit = await ctx.channel.send(".", delete_after=20 * 60)
 	server = ctx.guild
-	messages = await server_messages(server, to_edit)
-	# we save messages for fast reloading
-	with open(f"save_{server.id}.json", "w") as fmessages:
-		json.dump(messages, fmessages)
-		for x in messages:
-			finalmessages.append(x)
-	await add_server(server, messages)
-	await to_edit.edit(content=f"Done, loaded data from {len(server.channels)} channels!")
+	
+	preload = False
+	filepath = f"files/save_{server.id}.json"
+
+	if (os.path.isfile(filepath)):
+		preload = True
+
+	if not preload:
+		messages = await server_messages(server, to_edit)
+		# we save messages for fast reloading
+		with open(filepath, "w") as fmessages:
+			json.dump(messages, fmessages)
+		finalmessages = messages
+		await to_edit.edit(content=f"Done, loaded new data from {len(server.channels)} channels!")
+	else:
+		await to_edit.edit(content=f"Loading preloaded data...")
+		with open(filepath) as jsonfile:
+			data = json.load(jsonfile)
+			for x in data:
+				finalmessages.append((x[0], x[1]))
+		await to_edit.edit(content=f"Done, loaded preloaded data: {len(finalmessages)} messages!")
+	await add_server(server, finalmessages)
 	return finalmessages
+
 
 
 @slash.slash(name='cloud', description="Generate a worldcloud with your most recently said words!")
 async def cloud(ctx):
-	await ctx.defer()
-	server: discord.Guild = ctx.channel.guild
-	messages = await load(ctx)
-	
-	messages_filtered = [message for message in messages if message[0] == ctx.author.id]
+	try:
+		await ctx.defer()
+		server: discord.Guild = ctx.channel.guild
+		messages = await load(ctx)
 
-	counter = collections.Counter(messages_filtered)
+		print(f"TYPE IS ({type(messages)})")
 
-	members = []
-	# get all unique members targeted in this command
-	# members = set(ctx.message.mentions)
-	# for user_id in args:
-	# 	try:
-	# 		member = server.get_member(int(user_id))
-	# 		if member:
-	# 			members.add(member)
-	# 	except ValueError:
-	# 		pass
-	# if there's none it's for the author of the command
-	if not members:
-		members.append(ctx.author)
-	async with ctx.channel.typing():
-		for member in members:
-			wc = models[server].word_cloud(member.id)
-			if not wc:
-				await ctx.send(
-					content=f"Sorry I don't have any data on {member.mention} ...",
-					allowed_mentions=discord.AllowedMentions.none()
+		messages_filtered = [message for message in messages if message[0] == ctx.author.id]
+
+		counter = collections.Counter(messages_filtered)
+
+		members = []
+		# get all unique members targeted in this command
+		# members = set(ctx.message.mentions)
+		# for user_id in args:
+		# 	try:
+		# 		member = server.get_member(int(user_id))
+		# 		if member:
+		# 			members.add(member)
+		# 	except ValueError:
+		# 		pass
+		# if there's none it's for the author of the command
+		if not members:
+			members.append(ctx.author)
+		async with ctx.channel.typing():
+			for member in members:
+				wc = models[server].word_cloud(member.id)
+				if not wc:
+					await ctx.send(
+						content=f"Sorry I don't have any data on {member.mention} ...",
+						allowed_mentions=discord.AllowedMentions.none()
+					)
+					continue
+				result = await make_image.wc_image(
+					resolve_tags(server, wc),
+					emoji_resolver
 				)
-				continue
-			result = await make_image.wc_image(
-				resolve_tags(server, wc),
-				emoji_resolver
-			)
 
-			image = result[0]
-			text = f"{member.mention}'s Word Cloud: \n"
-			for item in counter.most_common(10):
-				text += f"**{item[0][1]}** - {item[1]} used \n"
-			
-			await ctx.send(
-				content=text, allowed_mentions=discord.AllowedMentions.none(),
-				file=discord.File(fp=image, filename=f"{member.display_name}_word_cloud.png")
-			)
+				image = result[0]
+				text = f"{member.mention}'s Word Cloud: \n"
+				for item in counter.most_common(10):
+					text += f"**{item[0][1]}** - {item[1]} used \n"
+				
+				text += f"Checked {len(messages_filtered)} messages"
+
+				await ctx.send(
+					content=text, allowed_mentions=discord.AllowedMentions.none(),
+					file=discord.File(fp=image, filename=f"{member.display_name}_word_cloud.png")
+				)
+	except:
+		print("FAILED CLOUD ERROR:", sys.exc_info()[0])
+		await ctx.send(content="Failed to create a worldcloud for you :(")
+		raise
 
 
 @cloud.error
